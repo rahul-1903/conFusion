33a7f0e246ce0fe46f8c55eb76be97dd62862e86
Rahul Kumar Gupta <rahulkumargupta19@gmail.com>
2018-08-23 22:56:13 +0530

    FAB Favorites

--- src/pages/dishdetail/dishdetail.ts
+++ src/pages/dishdetail/dishdetail.ts
@@ -2,7 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Dish } from '../../shared/dish';
 import { Comment } from '../../shared/comment';
-
+import { FavoriteProvider } from '../../providers/favorite/favorite';
 /**
  * Generated class for the DishdetailPage page.
  *
@@ -21,10 +21,13 @@ export class DishdetailPage {
   errMess: string;
   avgstars: string;
   numcomments: number;
+  favorite: boolean = false;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-    @Inject('BaseURL') private BaseURL) {
+    @Inject('BaseURL') private BaseURL,
+    private favoriteservice: FavoriteProvider) {
       this.dish = navParams.get('dish');
+      this.favorite = this.favoriteservice.isFavorite(this.dish.id);
       this.numcomments = this.dish.comments.length;
 
       let total = 0;
@@ -34,6 +37,11 @@ export class DishdetailPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DishdetailPage');
+  }
+
+  addToFavorites() {
+    console.log('Adding to Favorites', this.dish.id);
+    this.favorite = this.favoriteservice.addFavorite(this.dish.id);
   }
 
 }
